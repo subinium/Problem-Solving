e2c49381e706d8cc602765d9057b4190d84bbec7
subinium <subinium@gmail.com>
2022-02-23 11:57:10 +0900

    update leetcode 133

---
+++ leetcode/133-clone-graph.py
@@ -0,0 +1,13 @@
+from copy import deepcopy
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        return deepcopy(node)
