3364e009ed9ae86f5a258e6a6ea138ec3730d1a2
subinium <subinium@gmail.com>
2022-03-12 10:14:12 +0900

    update leetcode 138

---
+++ leetcode/138-copy-list-with-random-pointer.py
@@ -0,0 +1,13 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        return deepcopy(head)
