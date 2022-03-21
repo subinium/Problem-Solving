55844166aba675ed4edf931cda9b5b6b2cb8f1a6
subinium <subinium@gmail.com>
2022-03-21 10:16:05 +0900

    update leetcode 108, update763

---
+++ leetcode/108-convert-sorted-array-to-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return
+        mid = len(nums)//2
+        head = TreeNode(nums[mid])
+        head.left = self.sortedArrayToBST(nums[:mid])
+        head.right = self.sortedArrayToBST(nums[mid+1:])
+        return head
--- leetcode/763-partition-labels.py
+++ leetcode/763-partition-labels.py
@@ -1,9 +1,23 @@
+# class Solution:
+#     def partitionLabels(self, s: str) -> List[int]:
+#         st, ed = 0, 0
+#         ret = []
+#         for idx, c in enumerate(s):
+#             ed = max(ed, s.rfind(c))
+#             if ed == idx:
+#                 ret.append(ed-st+1)
+#                 st = idx+1
+#         return ret
+
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         st, ed = 0, 0
-        ret = []
+        ret, lst = [], defaultdict(int)
         for idx, c in enumerate(s):
-            ed = max(ed, s.rfind(c))
+            lst[c] = idx
+
+        for idx, c in enumerate(s):
+            ed = max(ed, lst[c])
             if ed == idx:
                 ret.append(ed-st+1)
                 st = idx+1
