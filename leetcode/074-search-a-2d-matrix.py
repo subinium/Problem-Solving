9bd4e227b13153c2ff82a89640cb5baef50b6427
subinium <subinium@gmail.com>
2022-03-30 10:22:18 +0900

    update leetcode 74

--- leetcode/074-search-a-2d-matrix.py
+++ leetcode/074-search-a-2d-matrix.py
@@ -1,7 +1,12 @@
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         for row in matrix:
+#             for val in row:
+#                 if val == target:
+#                     return True
+#         return False
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            for val in row:
-                if val == target:
-                    return True
-        return False
+        return any([target in row for row in matrix])
+                        
