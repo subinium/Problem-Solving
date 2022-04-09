4bb32e35be2def340d1dac7baaea6c43c885c4b8
subinium <subinium@gmail.com>
2022-04-09 10:08:48 +0900

    update leetcode 347

--- leetcode/347-top-k-frequent-elements.py
+++ leetcode/347-top-k-frequent-elements.py
@@ -3,3 +3,7 @@ class Solution:
         cnts = list(Counter(nums).items())
         cnts.sort(key=lambda x: x[1], reverse=True)
         return [num for num, _ in cnts[:k]]
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         return [n for n, _ in sorted(Counter(nums).items(), key=lambda x: -x[1])[:k]]
